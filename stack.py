a1a12583704f4d6d5fa571001a687e3811901fa3
mattling9 <mattling9@hotmail.co.uk>
2014-09-03 23:36:35 +0100

    Stack

--- stack.py
+++ stack.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+class Stack():
+    """A Class to represent a Stack"""
+    def __init__(self, MaxStackSize):
+        self._StackPointer = 0
+        self._MaxStackSize = 5
+
+def main():
+    print("1. Push data onto the stack")
+    print("2. Pull data from the stack")
+    print()
+    choice= int(input("Please choose a choice form the list above"))
+    if choice == 1:
+        
+        
