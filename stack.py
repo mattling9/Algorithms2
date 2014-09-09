1a4be3643e5101f40e6d851982216d3a0ec76524
mattling9 <mattling9@hotmail.co.uk>
2014-09-09 23:42:50 +0100

    Stack COMPLETE

--- stack.py
+++ stack.py
@@ -3,19 +3,66 @@ class Stack():
     def __init__(self, MaxSize):
         self.MaxSize = MaxSize
         self.StackPointer = 0
-        self.list = []
+        self.List = []
 
     def size(self):
-        StackSize = len(self.items)
+        StackSize = len(self.List)
         return StackSize
 
     def pop(self):
-        size = self.StackSize()
-        if Size > 0:
+        StackSize = self.StackSize()
+        if StackSize > 0:
             self.items.pop()
-            self.StackPointer = size
+            self.StackPointer = StackSize
         else:
             print("There are no items in the stack")
 
+
     def push(self, item ):
+        size()
+        StackSize = self.StackSize()
+        if self.StackSize < MaxSize:
+            self.List.append(item)
+            self.StackPoitner = StackSize
+        else:
+            print("The stack is full")
+
+    def peek(self):
+        size()
+        StackSize = self.StackSize
+        if StackSize == 0:
+            print(" the stack is empty")
+        else:
+            print("The top item is {0}".format(self.List[StackSize-1]))
+
+def main():
+    CustomStack = Stack(5)
+    Done = False
+    while Done == False:
+        print("Please select an option")
+        print()
+        print("1. Peek")
+        print("2. Push")
+        print("3. Pop")
+        print("4. Exit")
+        choice = int(input())
+        if choice == 1:
+            CustomStack.peek()
+        elif choice == 2:
+            CustomStack.push()
+        elif choice == 3:
+            CustomStack.pop()
+        elif choice == 4:
+            Done = True
+        while choice < 2 or choice > 4:
+            print("Please enter a valid number")
+            choice=int(input())
+
+if __name__ == "__main__":
+    main()
+
+
+    
+
+        
         
